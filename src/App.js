e6ac3299714c427d84c73d39b224c50fc8270257
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-12-06 12:36:00 +0200

    Add: Duck patter for folders and files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { React } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Book from './redux/books/books';
+import Categories from './redux/categories/categories';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+        <div>
+          <h1>Bookstore CMS</h1>
+          <nav className="navlist">
+            <ul>
+              <li>
+                <Link to="/">Books</Link>
+              </li>
+              <li>
+                <Link to="/categories">Categories</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Book />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/redux/books/books.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div className="books-main">
+      <h1>books</h1>
+    </div>
+  );
+}
+export default Book;
