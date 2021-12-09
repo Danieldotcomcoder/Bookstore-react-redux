b84d8839490ab442e8eb963c62fde478594811f4
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-12-09 14:33:32 +0200

    Fix: Books not appearing on load

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,8 +8,8 @@ export default function Book({ book }) {
   const dispatch = useDispatch();
   return (
     <li>
-      <span>{` ${book.title} `}</span>
-      <span>{` ${book.category} `}</span>
+      <h4>{` ${book.title} `}</h4>
+      <h4>{` ${book.category} `}</h4>
       <button type="button" onClick={() => dispatch(removeBookfromapi(book.item_id))}>Remove book</button>
     </li>
   );
